ec04e4b3a863522e82b49b0fa5fa983790d62f6f
max <max.smith@duke.edu>
2020-05-04 21:10:54 -0400

    tested crud routes on book quotes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -25,4 +25,29 @@ router.route('/add').post((req, res) => {
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
+router.route('/:id').get((req, res) => {
+    Book.findById(req.params.id)
+      .then(exercise => res.json(exercise))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/:id').delete((req, res) => {
+    Book.findByIdAndDelete(req.params.id)
+      .then(() => res.json('Book deleted.'))
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+  router.route('/update/:id').post((req, res) => {
+    Book.findById(req.params.id)
+      .then(book => {
+        book.username = req.body.username;
+        book.page = Number(req.body.page);
+        book.quote = req.body.quote;
+        book.thoughts = req.body.thoughts;
+  
+        book.save()
+          .then(() => res.json('Book updated!'))
+          .catch(err => res.status(400).json('Error: ' + err));
+      })
+      .catch(err => res.status(400).json('Error: ' + err));
+  });
+
 module.exports = router;
